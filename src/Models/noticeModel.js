e38f104f47dbe7b76a8f81faec1857e4b9562a9b
Keshav arora <keshavarora459@gmail.com>
2026-03-19 17:00:28 +0530

    Update noticeModel.js

--- src/Models/noticeModel.js
+++ src/Models/noticeModel.js
@@ -1,6 +1,20 @@
 import mongoose from 'mongoose';
 const {Schema} = mongoose
 const noticeSchema = new Schema({
+    title:{
+        type:String,
+        required:true},
+    description:{
+        type:String,
+        required:true},
+    date:{
+        type:Date,
+    },
+    level:{
+        type:String,
+        enum:['low','medium','high'],
+        default:'low'
+    }
    
 },{timestamps:true})
 const Notice = mongoose.model("Notice",noticeSchema);
